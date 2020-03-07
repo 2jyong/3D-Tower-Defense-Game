f7cbb386401f35b13c77fdfb79638dcfeadced68
Caaakey <kbop2000@gmail.com>
2020-03-07 18:04:26 +0900

    Update Game

---
+++ Assets/Enemys/Base/EnemyTransform.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class EnemyTransform : MonoBehaviour
+{
+    public float Speed;
+
+    private Transform checkPoint;
+    private int index = 1;
+
+    public void Start()
+    {
+        transform.position = WaypointManager.Get.GetWaypoint(0).position;
+        checkPoint = WaypointManager.Get.GetWaypoint(index);
+
+        SetDirection();
+    }
+
+    //  오브젝트의 이동이나 물리 연산의 경우 여기에 처리한다
+    private void FixedUpdate()
+    {
+        if (checkPoint == null) return;
+
+        //  Translate ? 이동시키는 함수
+        float point = (Speed * Time.deltaTime) * 1.5f;
+        transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, checkPoint.position) <= point)
+            NextPoint();
+    }
+
+    private void SetDirection()
+    {
+        //  내 위치와 상대 위치를 뺀 값을 정규화(단위벡터) 시켜준다
+        //  방향을 구하기 위해 사용. norvalVec = Vector / Vector Scale
+        Vector3 vec = (checkPoint.position - transform.position).normalized;
+        transform.rotation = Quaternion.LookRotation(vec);
+    }
+
+    private void NextPoint()
+    {
+        transform.position = checkPoint.position;
+        checkPoint = WaypointManager.Get.GetWaypoint(++index);
+
+        if (checkPoint == null)
+        {
+            //  Minus Life ..
+        }
+        else SetDirection();
+    }
+
+}
---
+++ Assets/Managers/SpawnManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnManager : MonoBehaviour
+{
+    public static SpawnManager Get { get; set; } = null;
+
+    public GameObject[] Prefabs;
+    public float RoundWaitTime = 10f;   //  끝나고 대기할 시간
+    public float SpawnTime = 1.5f;      //  한 마리 뽑을 때 마다 대기할 시간
+    public int SpawnLimit = 10;         //  최대 몬스터 개수
+
+    //  유니티와 시리얼라이즈를 하지 말라는 속성
+    [NonSerialized] public int count = 0;
+    private int monsterCount = 0;
+
+    private void Awake()
+    {
+        Get = this;
+    }
+
+    public void OnStart()
+    {
+        StartCoroutine(UpdateSpawnTimer(SpawnTime));
+    }
+
+    private IEnumerator UpdateSpawnTimer(float checkTime)
+    {
+        float currTime;
+        while (monsterCount != SpawnLimit)
+        {
+            currTime = Time.time;
+            while (Time.time - currTime <= checkTime)
+                yield return null;  //  한 프레임 쉬고 다시 실행
+
+            SpawnEnemy(0);
+        }
+
+        yield break;
+    }
+
+    private void SpawnEnemy(int index)
+    {
+        GameObject g = Instantiate(
+            Prefabs[index],
+            WaypointManager.Get.GetWaypoint(0).position,
+            Quaternion.identity,
+            null);
+
+        monsterCount++;
+    }
+
+}
